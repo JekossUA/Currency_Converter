c64f137e0cab6878ef9f56056aec20f8cf197bf1
Yevhenii Mykhailovskyi <mihaylovskiyeugeny@gmail.com>
2021-03-04 07:54:28 +0200

    all changes

---
+++ src/CurrencyConverter/CurrencyConverter/Converter.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using CurrencyConverter.Enums;
+using CurrencyConverter.Helpers;
+using CurrencyConverter.Models;
+
+namespace CurrencyConverter
+{
+    public class Converter
+    {
+        private string ApiKey { get; }
+
+        public Converter()
+        {
+        }
+
+        public Converter(string apiKey)
+        {
+            ApiKey = apiKey;
+        }
+
+        public double CurrencyConvert(double amount, CurrencyType from, CurrencyType to)
+        {
+            return RequestHelper.ExchangeRate(from, to, ApiKey) * amount;
+        }
+
+        public object amountEnter() 
+        {
+            try
+            {
+                bool isUserWrong = true;
+                while (isUserWrong) 
+                {
+                    Console.WriteLine("Enter converted amount");
+                    var input = Console.ReadLine();
+                    if (input is double)
+                    {
+                        return input;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Wrong value format");
+                        input = Console.ReadLine();
+                    }
+                }
+                
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            
+        }
+        public async Task<double> ConvertAsync(double amount, CurrencyType from, CurrencyType to)
+        {
+            return await Task.Run(() => CurrencyConvert(amount, from, to));
+        }
+
+
+        public List<Currency> GetAllCurrencies()
+        {
+            return RequestHelper.GetAllCurrencies(ApiKey);
+        }
+
+        public async Task<List<Currency>> GetAllCurrenciesAsync()
+        {
+            return await Task.Run(() => GetAllCurrencies());
+        }
+
+
+        public List<Country> GetAllCountries()
+        {
+            return RequestHelper.GetAllCountries(ApiKey);
+        }
+
+        public async Task<List<Country>> GetAllCountriesAsync()
+        {
+            return await Task.Run(() => GetAllCountries());
+        }
+
+
+        public CurrencyHistory GetHistory(CurrencyType from, CurrencyType to, DateTime date)
+        {
+            return RequestHelper.GetHistory(from, to, date.ToString("yyyy-MM-dd"), ApiKey);
+        }
+
+        public async Task<CurrencyHistory> GetHistoryAsync(CurrencyType from, CurrencyType to, DateTime date)
+        {
+            return await Task.Run(() => GetHistory(from, to, date.ToString("yyyy-MM-dd")));
+        }
+
+
+        public CurrencyHistory GetHistory(CurrencyType from, CurrencyType to, string date)
+        {
+            return RequestHelper.GetHistory(from, to, date, ApiKey);
+        }
+
+        public async Task<CurrencyHistory> GetHistoryAsync(CurrencyType from, CurrencyType to, string date)
+        {
+            return await Task.Run(() => GetHistory(from, to, date));
+        }
+
+
+        public List<CurrencyHistory> GetHistoryRange(CurrencyType from, CurrencyType to, string startDate, string endDate)
+        {
+            return RequestHelper.GetHistoryRange(from, to, startDate, endDate, ApiKey);
+        }
+
+        public async Task<List<CurrencyHistory>> GetHistoryRangeAsync(CurrencyType from, CurrencyType to, string startDate, string endDate)
+        {
+            return await Task.Run(() => GetHistoryRange(from, to, startDate, endDate));
+        }
+
+
+        public List<CurrencyHistory> GetHistoryRange(CurrencyType from, CurrencyType to, DateTime startDate, DateTime endDate)
+        {
+            return GetHistoryRange(from, to, startDate.ToString("yyyy-MM-dd"), endDate.ToString("yyyy-MM-dd"));
+        }
+
+        public async Task<List<CurrencyHistory>> GetHistoryRangeAsync(CurrencyType from, CurrencyType to, DateTime startDate, DateTime endDate)
+        {
+            return await Task.Run(() => GetHistoryRange(from, to, startDate.ToString("yyyy-MM-dd"), endDate.ToString("yyyy-MM-dd")));
+        }
+
+    }
+}
---
+++ src/CurrencyConverter/CurrencyConverterConsole/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Linq;
+using CurrencyConverter;
+using CurrencyConverter.Enums;
+
+namespace CurrencyConverterConsole
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            //Converter object
+            /*var premiumConverter = new Converter("e672ae922ef1dda24627");*/
+            var converter = new Converter("e672ae922ef1dda24627");
+
+            //List of currencies
+            /*var currencies = converter.GetAllCurrencies();
+            Console.WriteLine(string.Join(",", currencies.Select(p => p.Id).ToArray()));*/
+
+            //List of countries
+            /*var countries = converter.GetAllCountries();
+            Console.WriteLine(string.Join(",", countries.Select(p => p.Name).ToArray()));*/
+
+            ////Basic conversion
+            double currency = converter.amountEnter();
+            var basic = converter.CurrencyConvert(currency, CurrencyType.USD, CurrencyType.EUR);
+            Console.WriteLine(basic);
+
+            ////History Single Date
+            //var history = converter.GetHistory(CurrencyType.USD, CurrencyType.EUR, "2018-08-01");
+            //Console.WriteLine(history.Date);
+            //Console.WriteLine(history.ExchangeRate);
+
+            ////History Date Range
+            //var historyRange = converter.GetHistoryRange(CurrencyType.USD, CurrencyType.EUR, "2018-08-01", "2018-08-06");
+            //Console.WriteLine(string.Join(",", historyRange.Select(p => p.Date).ToArray()));
+            //Console.WriteLine(string.Join(",", historyRange.Select(p => p.ExchangeRate).ToArray()));
+
+            //Async Functions
+            //GetAllCurrenciesAsync(converter);
+            //GetAllCountriesAsync(converter);
+            //ConvertAsync(converter);
+            //GetHistoryAsync(converter);
+            //GetHistoryRangeAsync(converter);
+
+            Console.Read();
+        }
+
+        //List of currencies Async
+        private static async void GetAllCurrenciesAsync(Converter converter)
+        {
+            var result = await converter.GetAllCurrenciesAsync();
+            Console.WriteLine(string.Join(",", result.Select(p => p.Id).ToArray()));
+        }
+
+        //List of currencies Async
+        private static async void GetAllCountriesAsync(Converter converter)
+        {
+            var result = await converter.GetAllCountriesAsync();
+            Console.WriteLine(string.Join(",", result.Select(p => p.Name).ToArray()));
+        }
+
+        //Basic Conversion Async
+        private static async void ConvertAsync(Converter converter)
+        {
+            var result = await converter.ConvertAsync(1, CurrencyType.USD, CurrencyType.EUR);
+            Console.WriteLine("Exchange rate : {0}", result);
+        }
+
+        //History Single Date Async
+        private static async void GetHistoryAsync(Converter converter)
+        {
+            var history = await converter.GetHistoryAsync(CurrencyType.USD, CurrencyType.EUR, "2018-08-01");
+            Console.WriteLine(history.Date);
+            Console.WriteLine(history.ExchangeRate);
+        }
+
+        //History Date Range Async
+        private static async void GetHistoryRangeAsync(Converter converter)
+        {
+            var historyRange = await converter.GetHistoryRangeAsync(CurrencyType.USD, CurrencyType.EUR, "2018-08-01", "2018-08-06");
+            Console.WriteLine(string.Join(",", historyRange.Select(p => p.Date).ToArray()));
+            Console.WriteLine(string.Join(",", historyRange.Select(p => p.ExchangeRate).ToArray()));
+        }
+    }
+}
